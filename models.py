25b0a46b90f3c474d83a4f72004e499605bfb357
Yogesh Jean <yogeshmathi12@gmail.com>
2020-08-15 00:30:14 +0530

    Uploading after some modification

--- models.py
+++ models.py
@@ -1,89 +1,122 @@
-from app import db
+from app import *
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 
-class User:
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    user_name = Column(String, nullable=False)
+    password = Column(String, nullable=False)
 
     def __init__(self, user_name, password):
         self._user_name = user_name
         self._password = password
+        self._user = db_session.query(User).filter_by(user_name=self._user_name).first()
 
-    def is_username_valid(self):
-
-        _user_name = db.execute('select user_name '
-                               'from users '
-                               'where user_name = \'{}\' '.format(self._user_name))
-
-        if _user_name.fetchone() is None:
-            return False
-        else:
+    def is_valid(self):
+        if self._user.user_name == self._user_name and self._user.password == self._password:
             return True
-
-    def is_password_valid(self):
-        _user_password = db.execute('select * '
-                                   'from users '
-                                   'where password =  \'{}\''.format(self._password))
-
-        if _user_password.fetchone() is None:
-            return False
         else:
-            return True
+            return False
 
     def get_user_id(self):
-        _user_id = db.execute('select user_id '
-                             'from users '
-                             'where user_name = \'{}\' '.format(self._user_name))
-        result = _user_id.fetchone()[0]
-        return result
+        return self._user.user_id
 
 
-class Category:
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
 
     def __init__(self, category_id):
         self._category_id = category_id
+        self._category = db_session.query(Category).filter_by(id=self._category_id).first()
 
     def get_category_name(self):
-        category_name = db.execute('select name from categories where id = \'{}\''.format(self._category_id))
-        return category_name.fetchone()[0]
+        return self._category.name
+
+    def get_category_details(self):
+        category_dict = {}
+        category_dict['category_id'] = self._category.id
+        category_dict['category_name'] = self._category.name
+        return category_dict
 
 
-class Seller:
+class Seller(Base):
+    __tablename__ = 'sellers'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
 
     def __init__(self, seller_id):
         self._seller_id = seller_id
+        self._seller = db_session.query(Seller).filter_by(id=self._seller_id).first()
 
     def get_seller_name(self):
-        seller_name = db.execute('select name from sellers where id = \'{}\''.format(self._seller_id))
-        return seller_name.fetchone()[0]
+        return self._seller.name
 
 
-class Product:
+class Product(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    price = Column(Integer, nullable=False)
+    category_id = Column(Integer, ForeignKey('categories.id'))
+    seller_id = Column(Integer, ForeignKey('sellers.id'))
+    quantity = Column(Integer, nullable=False)
 
     def __init__(self, product_id):
         self._product_id = product_id
+        self._product = db_session.query(Product).filter_by(id=self._product_id).first()
+
+    def get_product_details(self):
+        product_dict = {}
+        product_dict['product_id'] = self._product.id
+        product_dict['product_name'] = self._product.name
+        product_dict['price'] = self._product.price
+        product_dict['category_name'] = Category(self._product.category_id).get_category_name()
+        product_dict['seller_name'] = Seller(self._product.seller_id).get_seller_name()
+        product_dict['quantity'] = self._product.quantity
+        return product_dict
 
     def quantity_check(self, quantity):
-        stock = db.execute('select quantity from products where id = \'{}\''.format(self._product_id))
-        if stock.fetchone()[0] >= (int(quantity)):
+        if self._product.quantity >= (int(quantity)):
             return True
         else:
             return False
 
 
-class Cart:
+class Cart(Base):
+    __tablename__ = 'carts'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('users.user_id'))
 
-    def __init__(self, user_id):
-        self._user_id = user_id
+    def __init__(self, _user_id):
+        self._user_id = _user_id
+        self._cart = db_session.query(Cart).filter_by(user_id=self._user_id).first()
 
     def get_cart_id(self):
-        cart_id = db.execute('select id from carts where user_id = \'{}\''.format(self._user_id))
-        return cart_id.fetchone()[0]
+        return self._cart.id
 
 
-class CartProduct:
+class CartProduct(Base):
+    __tablename__ = 'cart_products'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    cart_id = Column(Integer, ForeignKey('carts.id'))
+    product_id = Column(Integer, ForeignKey('products.id'))
+    product_quantity = Column(Integer, nullable=False)
 
     def __init__(self, cart_id, product_id):
         self._cart_id = cart_id
         self._product_id = product_id
+        self._cart_product = db_session.query(CartProduct).filter_by(cart_id=self._cart_id, product_id=self._product_id).first()
 
     def add_product_to_cart(self):
         db.execute('insert into cart_products '
@@ -96,66 +129,50 @@ class CartProduct:
         return new_id
 
     def remove_product_from_cart(self):
-        db.execute('delete from cart_products '
-                   'where cart_id = \'{}\' and product_id = \'{}\''.format(self._cart_id, self._product_id))
+        db_session.delete(self._cart_product)
+        db_session.commit()
         return True
 
     def update_product_quantity_in_cart(self, quantity):
-        db.execute('update cart_products set product_quantity = \'{}\' '
-                   'where cart_id =\'{}\' and product_id = \'{}\''.format(quantity, self._cart_id, self._product_id))
+        self._cart_product.product_quantity = quantity
+        db_session.add(self._cart_product)
+        db_session.commit()
         return True
 
     def get_cart_product_quantity(self):
-        ordered_quantity = db.execute('select product_quantity '
-                                      'from cart_products '
-                                      'where cart_id = \'{}\' and product_id = \'{}\''.format(self._cart_id, self._product_id))
-        result = ordered_quantity.fetchone()[0]
-        return result
+        return self._cart_product.product_quantity
 
 
 def get_all_categories():
-    categories = db.execute('select * from categories')
-    result = [dict(row) for row in categories.fetchall()]
-    return result
+    categories = db_session.query(Category).all()
+    category_list = []
+    for category in categories:
+        category_list.append(Category(category.id).get_category_details())
+    return category_list
 
 
 def get_all_products(category_id):
-    products = db.execute('select * from products where category_id = \'{}\''.format(category_id))
-    result = [dict(row) for row in products.fetchall()]
-    return result
+    product_list = []
+    products = db_session.query(Product).filter_by(category_id=category_id).all()
+    for product in products:
+        product_list.append(Product(product.id).get_product_details())
+    return product_list
 
 
 def get_cart_products(cart_id):
-    products = db.execute('select product_id '
-                          'from cart_products '
-                          'where cart_id = \'{}\' '.format(cart_id))
-    result = [dict(row) for row in products.fetchall()]
+    cart_product_ids = db_session.query(CartProduct).filter_by(cart_id=cart_id).all()
     cart_product_details = []
-    for i in range(len(result)):
-        cart_product_details.append(get_cart_product_details(result[i]['product_id'], cart_id))
-
+    for i in range(len(cart_product_ids)):
+        cart_product_details.append(get_cart_product_details(cart_product_ids[i].product_id, cart_id))
     return cart_product_details
 
 
 def get_cart_product_details(product_id, cart_id):
-    details = db.execute('select id, name, price, seller_id, category_id '
-                         'from products '
-                         'where id = \'{}\''.format(product_id))
+    cart_products = db_session.query(Product).filter_by(id=product_id).all()
+    for row in cart_products:
+        product_details = Product(row.id).get_product_details()
+        product_details['quantity'] = CartProduct(cart_id, row.id).get_cart_product_quantity()
+    return product_details
 
-    for row in details.fetchall():
-        result = __formate(row, cart_id)
-
-    return result
-
-
-def __formate(row, cart_id):
-    dic = {}
-    dic['product_id'] = row.id
-    dic['name'] = row.name
-    dic['price'] = row.price
-    dic['seller_name'] = Seller(row.seller_id).get_seller_name()
-    dic['category_name'] = Category(row.category_id).get_category_name()
-    dic['ordered_quantity'] = CartProduct(cart_id, row.id).get_cart_product_quantity()
-    return dic
 
 
--- routes.py
+++ routes.py
@@ -2,39 +2,38 @@ from app import *
 from models import *
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 def login():
 
     if request.method == 'POST':
         user_name = request.form['user_name']
         password = request.form['password']
         user = User(user_name, password)
-
-        if user.is_username_valid():
-            if user.is_password_valid():
-                session['login_status'] = True
-                session['user_id'] = user.get_user_id()
-                return 'Login successful'
-            else:
-                return 'Invalid username or password'
+        if user.is_valid() == True:
+            session['user_id'] = user.get_user_id()
+            return 'Login'
         else:
             return 'Invalid username or password'
 
 
+
 @app.route('/categories', methods=['GET'])
 def display_categories():
+
     categories = get_all_categories()
     return jsonify(categories)
 
 
 @app.route('/categories/<category_id>/products', methods=['GET'])
 def display_products(category_id):
+
     products = get_all_products(category_id)
     return jsonify(products)
 
 
 @app.route('/cart/<user_id>', methods=['GET'])
 def view_cart(user_id):
+
     cart_id = Cart(user_id).get_cart_id()
     result = get_cart_products(cart_id)
     return jsonify(result)
@@ -42,6 +41,7 @@ def view_cart(user_id):
 
 @app.route('/cart/<user_id>', methods=['DELETE'])
 def delete_product_from_cart(user_id):
+
     product_id = request.form['product_id']
     cart_id = Cart(user_id).get_cart_id()
     CartProduct(cart_id, product_id).remove_product_from_cart()
@@ -50,6 +50,7 @@ def delete_product_from_cart(user_id):
 
 @app.route('/cart/<user_id>', methods=['POST'])
 def add_to_cart(user_id):
+
     product_id = request.form['product_id']
     cart_id = Cart(user_id).get_cart_id()
     CartProduct(cart_id, product_id).add_product_to_cart()
@@ -73,6 +74,4 @@ def logout():
     session.pop('user_id', None)
     session.pop('login_status', None)
     return 'Logged out'
-
-
 
