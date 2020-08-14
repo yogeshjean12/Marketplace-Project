0d75f20a8831b3cb72da2ddfe119bedbde3595ed
Yogesh Jean <yogeshmathi12@gmail.com>
2020-08-14 19:15:04 +0530

    Updated app file

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask, request, jsonify, session
 from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session,sessionmaker
 
 
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'NOTHING-SECRET'
+app.config['SECRET_KEY'] = 'YOUR-SECRET'
 
 db = create_engine("postgres+psycopg2://postgres:yogesh5201@localhost/project")
-
+db_session = scoped_session(sessionmaker(bind=db))
 
 from routes import *
 
 
 if __name__ == '__main__':
-    app.run(debug=
+    app.run(debug=True)
