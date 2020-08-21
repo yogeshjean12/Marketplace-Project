e36cbd6230fd187a96b721c3cda341f2b3042917
Yogesh Jean <yogeshmathi12@gmail.com>
2020-08-21 22:08:33 +0530

    uploading category file

---
+++ category.py
@@ -0,0 +1,23 @@
+from app import *
+
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+
+    def __init__(self, category_id):
+        self._category_id = category_id
+
+    def get_category_name(self):
+        _category = db_session.query(Category).filter_by(id=self._category_id).first()
+        return _category.name
+
+    def get_category_details(self):
+        _category = db_session.query(Category).filter_by(id=self._category_id).first()
+        category_dict = {}
+        category_dict['category_id'] = _category.id
+        category_dict['category_name'] = _category.name
+        return category_dict
+
