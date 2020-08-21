7c391be817ffdaab2b8ef1642cbd5252aa24bad1
Yogesh Jean <yogeshmathi12@gmail.com>
2020-08-21 22:12:20 +0530

    uploading user file

---
+++ user.py
@@ -0,0 +1,47 @@
+from app import *
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    user_name = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+
+    def __init__(self, user_name, password):
+        self._user_name = user_name
+        self._password = password
+
+    def is_valid(self):
+        _user = db_session.query(User).filter_by(user_name=self._user_name).first()
+        if _user is not None:
+            if _user.user_name == self._user_name and _user.password == self._password:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def get_user_id(self):
+        _user = db_session.query(User).filter_by(user_name=self._user_name).first()
+        return _user.user_id
+
+    def register_user(self):
+        if self.__is_username_exist() == False:
+            db.execute('insert into users '
+                    'values(\'{}\',\'{}\',\'{}\')'.format(self.__get_new_id(), self._user_name, self._password))
+            return True
+        else:
+            return False
+
+    def __is_username_exist(self):
+        _user = db_session.query(User).filter_by(user_name=self._user_name).first()
+        if _user is None:
+            return False
+        else:
+            return True
+
+    def __get_new_id(self):
+        id = db.execute('select max(user_id) from users')
+        new_id = (id.fetchone()[0]) + 1
+        return new_id
